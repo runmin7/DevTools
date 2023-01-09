ee099a03c98e277843ef9806e8f917412c39efba
runmin7 <runmin@naver.com>
2023-01-09 18:56:04 +0900

    start sample

---
+++ src/main/java/com/lab/core/file/Hello.java
@@ -0,0 +1,26 @@
+package com.lab.core.file;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.restservice.Greeting;
+
+@RestController
+public class Hello {
+
+	private static final String template = "Hello, %s!";
+	private final AtomicLong counter = new AtomicLong();
+
+	@GetMapping("/greeting")
+	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+	}
+	
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+	}
+}
